9d10ad66b8c9d39ff9fb1c8e05af23ed0e26e75a
lucas.previato <lucaspreviato@live.com>
2025-04-23 00:20:15 -0300

    feat(cli): estrutura inicial do main.rs com parser de comandos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod cli;
+mod app;
+
+use cli::commands::build_cli;
+use app::build_cache;
+
 fn main() {
-    println!("Hello, world!");
+    let  matches = build_cli().get_matches();
+
+    match matches.subcommand(){
+        Some(("build", _sub_matches)) => {
+
+            build_cache::run();
+        }
+        Some(("clean", _sub_matches)) => {
+            println!("Cleaning cache");//TODO: Implement clean command
+        }
+        _ => {
+            println!("use --help to see available commands");
+        }
+    }
 }
